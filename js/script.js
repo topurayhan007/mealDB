8fd4cc043ec25e66cd17694ca62a30d05484d7ae
topurayhan007 <topurayhan007@gmail.com>
2023-03-01 22:17:26 +0600

    all functionality done except modal

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
-const loadMeals = async (searchText) => {
+const loadMeals = async (searchText, index) => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  // console.log("asdsa", url);
   try {
     const res = await fetch(url);
     const data = await res.json();
-    displayMeals(data.meals);
+    displayMeals(data.meals, index);
   } catch (error) {
+    alert("No food found");
+
     console.log(error);
   }
 };
 
-const displayMeals = (meals) => {
+const displayMeals = (meals, index) => {
   const mealsContainer = document.getElementById("meals-container");
+  const showAllButton = document.getElementById("btn-show-all");
   mealsContainer.innerHTML = "";
+  if (index < meals.length) {
+    meals = meals.slice(0, index);
+    showAllButton.classList.remove("hidden");
+  } else {
+    showAllButton.classList.add("hidden");
+  }
 
   meals.forEach((meal) => {
     const mealDiv = document.createElement("div");
+
     mealDiv.innerHTML = `
     <div
             class="flex md:flex-col lg:flex-row items-center bg-white border border-2 border-gray-200 rounded-xl hover:bg-gray-50"
           >
             <img
               src="${meal.strMealThumb}"
-              class="object-cover w-1/2 md:w-full rounded-t-lg rounded-b-lg lg:h-72"
+              class="object-cover w-1/2 md:w-full rounded-t-lg rounded-b-lg h-56 lg:h-72"
               alt=""
             />
             <div
@@ -50,9 +61,18 @@ const displayMeals = (meals) => {
   });
 };
 
-const searchMeals = () => {
+const returnSearchText = () => {
   const searchText = document.getElementById("search-field").value;
-  loadMeals(searchText);
+  return searchText;
 };
 
-loadMeals("pie");
+const searchMeals = () => {
+  const searchText = returnSearchText();
+  loadMeals(searchText, 6);
+};
+
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  loadMeals(returnSearchText());
+});
+
+loadMeals("", 6);
